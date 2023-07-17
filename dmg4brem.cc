76e0899b82d199895daf4a67b5c7b904e15648f3
Gajendra Gurung <gajendra.315.gurung@gmail.com>
2023-07-16 19:00:44 -0500

    implemented -s -o -m options for running with input seed, name of output file and select macro file

--- dmg4brem.cc
+++ dmg4brem.cc
@@ -34,129 +34,120 @@
 #include "DarkMatterParametersFactory.hh"
 #include "Randomize.hh"
 
-int main(int argc,char** argv) {
+#include <unistd.h>
+#include <cstdlib>
 
- G4long seed = 132334;
- G4UIExecutive* ui = 0;
- G4String outputFileName;
- G4String* foutputfFileName = &outputFileName; 
- 
- switch(argc)
- { 
-	 case 1:
-		 G4cout << "Interactive mode" << G4endl;
-		 ui = new G4UIExecutive(argc, argv);
-		 break;
-	 case 2:
-		 G4cout << "Single job mode : set random seed to be " << seed << "." << G4endl;
-		 G4Random::setTheSeed(seed);
-		 break;
-	 case 3:
-		 G4cout << "Batch job mode" << G4endl;
-		 outputFileName = argv[2];
-		 G4Random::setTheEngine(new CLHEP::MTwistEngine);
-		 G4Random::setTheSeed(seed);
-		 break;
-         case 4: 
-		 G4cout << "Batch mode with input random seed" << G4endl;
-                 outputFileName = argv[2];
-                 seed = atol(argv[3]);
-                 G4Random:: setTheSeed(seed);
-                 G4cout << "The seed is set to: " << seed << G4endl;
-		 break;
-	 default:
-		 G4cout << "Interactive Mode" << G4endl;
-		 ui = new G4UIExecutive(argc, argv);
-		 break;
- }
-
-
-  // Run manager
-#ifdef G4MULTITHREADED
-  G4MTRunManager* runManager = new G4MTRunManager;
-  G4int nThreads = G4Threading::G4GetNumberOfCores();
-  runManager->SetNumberOfThreads(nThreads);
-#else
-  G4RunManager * runManager = new G4RunManager;
-#endif
-
-  // UserInitialization classes
-  DetectorConstruction* detectorConstruction = new DetectorConstruction;
-  runManager->SetUserInitialization(detectorConstruction);
-
-  // ___ Here the "extension" part starts ___
-  G4PhysListFactory factory;
-  G4VModularPhysicsList * phys = factory.GetReferencePhysList("FTFP_BERT");
-  // ^^^ most of the standard physics lists are available by this interface
-
- // G4PhysicsListHelper * phLHelper = G4PhysicsListHelper::GetPhysicsListHelper();
- // phLHelper->DumpOrdingParameterTable();
-
-  DarkMatterPhysics* darkMatterPhysics = new DarkMatterPhysics();
-  //bool config = darkMatterPhysics->DarkMatterPhysicsConfigure();
-  DarkMatterParametersFactory* DMpar = DarkMatterParametersFactory::GetInstance();
-  G4cout << "BiasSigmaFactor0 = " << DMpar->GetRegisteredParam("BiasSigmaFactor0") << G4endl;
-  G4cout << "EThresh = " << DMpar->GetRegisteredParam("EThresh")/CLHEP::MeV << G4endl;
-  G4cout << "DMProcessType = " << DMpar->GetRegisteredParam("DMProcessType") << G4endl;
-  G4cout << "DMMass = " << DMpar->GetRegisteredParam("DMMass")/CLHEP::MeV << G4endl;
-  G4cout << "Epsilon = " << DMpar->GetRegisteredParam("Epsilon") << G4endl;
-  phys->RegisterPhysics(darkMatterPhysics);
-  // ^^^ Here the "extension" part ends ^^^
-
-  runManager->SetUserInitialization(phys);  // init phys
-
-  // User action initialization
-  switch(argc)
-  { 
-	case 1:
-		runManager->SetUserInitialization(new ActionInitialization(detectorConstruction, darkMatterPhysics->GetDarkMatterPointer()));
-		break;
- 	case 2:
-		runManager->SetUserInitialization(new ActionInitialization(detectorConstruction, darkMatterPhysics->GetDarkMatterPointer()));
-		break;
- 	case 3:
-		runManager->SetUserInitialization(new ActionInitialization(detectorConstruction, darkMatterPhysics->GetDarkMatterPointer(), foutputfFileName));
-		break;
- 	case 4:
-		runManager->SetUserInitialization(new ActionInitialization(detectorConstruction, darkMatterPhysics->GetDarkMatterPointer(), foutputfFileName));
-		break;
-        default:
-		runManager->SetUserInitialization(new ActionInitialization(detectorConstruction, darkMatterPhysics->GetDarkMatterPointer()));
-		break;
-  }
-
-
-#ifdef G4VIS_USE
-  // Visualization, if you choose to have it!
-  G4VisManager* visManager = new G4VisExecutive;
-  visManager->Initialize();
-#endif
-
-
-  // User interactions
-  G4UImanager* UImanager = G4UImanager::GetUIpointer();  
-
-  if(!ui)
-  { // batch mode
-    G4String command = "/control/execute ";
-    G4String fileName = argv[1];
-    UImanager->ApplyCommand(command+fileName);
-	    
-  }
-  else
-  // vis mode
-  { 
-    // G4UIterminal is a (dumb) terminal.
-    UImanager->ApplyCommand("/control/execute vis.mac");    
-    ui->SessionStart();
-    delete ui;
-  }
-
-#ifdef G4VIS_USE
-  delete visManager;
-#endif
-  delete runManager;
-
-  return 0;
+void print_usage(){
+  G4cerr << "Usage: " << G4endl;
+  G4cerr << "   -m [macro (input) file name]" << G4endl;
+  G4cerr << "   -o [output (date) file name]" << G4endl;
+  G4cerr << "   -s [random number randSeed]"  << G4endl;
 }
+
+int main(int argc,char** argv){
+  
+  int optC = 0;
+  
+  G4long randSeed = time(NULL);
+  G4UIExecutive* ui = 0;
+  G4String macroFilename, dataFilename;
+  G4String* dataFilename_ptr = &dataFilename;
+  while((optC = getopt(argc, argv, "m:o:s:"))!= -1) {
+      switch(optC){
+      case 'm':
+        macroFilename = optarg;
+        break;
+
+      case 'o':
+        dataFilename = optarg;
+        break;
+
+      case 's':
+        randSeed = atol(optarg);
+        break;
+
+	  case ':':
+	  case '?':
+		print_usage();
+	    exit(EXIT_FAILURE);
+		break;
+	  }
+  }
+
+  G4Random::setTheEngine(new CLHEP::MTwistEngine);
+  G4Random::setTheSeed(randSeed);
+  //G4Random::showEngineStatus();
+	
+	// Run manager
+#ifdef G4MULTITHREADED
+	G4MTRunManager* runManager = new G4MTRunManager;
+	G4int nThreads = G4Threading::G4GetNumberOfCores();
+	runManager->SetNumberOfThreads(nThreads);
+#else
+	G4RunManager * runManager = new G4RunManager;
+#endif
+	
+	// UserInitialization classes
+	DetectorConstruction* detectorConstruction = new DetectorConstruction;
+	runManager->SetUserInitialization(detectorConstruction);
+	
+	// ___ Here the "extension" part starts ___
+	G4PhysListFactory factory;
+	G4VModularPhysicsList * phys = factory.GetReferencePhysList("FTFP_BERT");
+	// most of the standard physics lists are available by this interface
+	
+	// G4PhysicsListHelper * phLHelper = G4PhysicsListHelper::GetPhysicsListHelper();
+	// phLHelper->DumpOrdingParameterTable();
+	
+	DarkMatterPhysics* darkMatterPhysics = new DarkMatterPhysics();
+	// bool config = darkMatterPhysics->DarkMatterPhysicsConfigure();
+	DarkMatterParametersFactory* DMpar = DarkMatterParametersFactory::GetInstance();
+	G4cout << "BiasSigmaFactor0 = " << DMpar->GetRegisteredParam("BiasSigmaFactor0") << G4endl;
+	G4cout << "EThresh = " << DMpar->GetRegisteredParam("EThresh")/CLHEP::MeV << G4endl;
+	G4cout << "DMProcessType = " << DMpar->GetRegisteredParam("DMProcessType") << G4endl;
+	G4cout << "DMMass = " << DMpar->GetRegisteredParam("DMMass")/CLHEP::MeV << G4endl;
+	G4cout << "Epsilon = " << DMpar->GetRegisteredParam("Epsilon") << G4endl;
+	phys->RegisterPhysics(darkMatterPhysics);
+	// ^^^ Here the "extension" part ends ^^^
+	
+	runManager->SetUserInitialization(phys);  // init phys
+  
+  if(!dataFilename.empty())
+  {
+	runManager->SetUserInitialization(new ActionInitialization(detectorConstruction, darkMatterPhysics->GetDarkMatterPointer(), dataFilename_ptr));}
+  else {
+	runManager->SetUserInitialization(new ActionInitialization(detectorConstruction, darkMatterPhysics->GetDarkMatterPointer()));
+  }
+
+	
+#ifdef G4VIS_USE
+	// Visualization, if you choose to have it!
+	G4VisManager* visManager = new G4VisExecutive;
+	visManager->Initialize();
+#endif
+	
+	// User interactions
+	G4UImanager* UImanager = G4UImanager::GetUIpointer();
+	
+	if(!ui)
+	{ // batch mode
+	  G4String command = "/control/execute ";
+	  UImanager->ApplyCommand(command+macroFilename);
+	  
+	}
+	else
+	{
+      ui = new G4UIExecutive(argc,argv);
+	  UImanager->ApplyCommand("/control/execute vis.mac");
+	  ui->SessionStart();
+	  delete ui;
+	}
+	
+#ifdef G4VIS_USE
+	delete visManager;
+#endif
+	delete runManager;
+	
+	return 0;
+  }
 
